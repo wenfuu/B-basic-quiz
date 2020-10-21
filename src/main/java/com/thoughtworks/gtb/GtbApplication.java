bd0238fa647c57856686dc968d4d0200589b15ec
Wen Fu <wen.fu@thoughtworks.com>
2020-10-21 11:34:59 +0800

    [Wen Fu] init.

---
+++ src/main/java/com/thoughtworks/gtb/GtbApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.gtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtbApplication.class, args);
+	}
+
+}
